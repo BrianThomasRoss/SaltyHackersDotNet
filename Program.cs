1e9b1f4116b8163ccd42818ca599918996473f2f
Brian Thomas Ross <ml@brianthomasross.com>
2020-08-13 15:46:25 -0400

    Initialize ML.NET project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaltyHackers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
